1c62abe7ce363a72cccfbd8a84ca219af53c517f
DI-just-code <54669185+DI-just-code@users.noreply.github.com>
2019-08-29 18:46:09 +0600

    start
    start

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,8 +5,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello CodeSandbox</h1>
-        <h2>Start editing to see some magic happen!</h2>
+        <h1>Hello World!</h1>
       </div>
     );
   }
