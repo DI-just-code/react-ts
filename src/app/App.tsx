6c4ca82d161ab7838ecdd3b7ffff094a1ec00ac2
DI-just-code <54669185+DI-just-code@users.noreply.github.com>
2019-08-29 18:10:40 +0600

    Initial commit

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </div>
+    );
+  }
+}
+export default App;
